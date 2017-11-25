b8340a408d59fc3df9bf9341b2b444447b65e820
Antoine Caillon <caillonantoine@gmail.com>
2017-11-25 23:45:06 +0000

    Passage en 2-dimension, (+) get_Wall_eq

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 #coding:utf-8
-
+from __future__ import division
 import numpy as np
 import matplotlib.pyplot as plt
 
 class Solid(object):
     pass
-    
+
 class Wall(Solid):
-    def __init__(self,start,end):
+    def __init__(self,eq,start,length):
+        """Bi-dimensionnal"""
+        self.eq = eq
         self.start = start
-        self.end = end
-    def get_collision_instant(self,x0,t0,cd):
+        self.length = length
         
+def get_Wall_eq(start_point,end_point):
+    """Bi-dimensionnal"""
+    if start_point[0] == end_point[0]:
+        return lambda x:(start_point[0],x)
+    elif start_point[1] == end_point[1]:
+        return lambda x:(x,start_point[1])
+    else:
+        a = (start_point[1] - end_point[1])/(start_point[0] - end_point[0])
+        b = start_point[1] - a*start_point[0]
+        return lambda x:(x,a*x+b)
